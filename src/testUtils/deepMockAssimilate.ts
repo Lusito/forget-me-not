03758d13121ac75cc67bcaaa19d8b832bf1fe15f
Lusito <core@lusito.info>
2020-04-26 20:21:46 +0200

    extra validation

--- src/testUtils/deepMockAssimilate.ts
+++ src/testUtils/deepMockAssimilate.ts
@@ -32,9 +32,27 @@ export function mockAssimilate<T extends { [s: string]: any }, TKey extends stri
     mockKeys: TKey[],
     whitelist?: string[]
 ): AssimilatedMockMap<Pick<T, TKey>> {
-    const [proxy, mock] = quickDeepMock<T>("assimilated"); // fixme: name
-    // fixme: validate, that every part of mocks is in property
-    for (const property of getProperties(instance)) {
+    const name = "assimilated";// fixme: name
+    const [proxy, mock] = quickDeepMock<T>(name);
+    const properties = getProperties(instance);
+    
+    // Validate, that every mock is in properties
+    for(const key of mockKeys) {
+        if (!properties.includes(key as TKey))
+            throw new DeepMockError(`Property "${key}" does not exist on ${name}, so it can't be assimilated`);
+        else if (typeof instance[key] !== "function")
+            throw new DeepMockError(`Property "${key}" on ${name} is not a function, so it can't be assimilated`);
+    }
+    
+    // Validate, that every whitelist item is in properties
+    if (whitelist) {
+        for(const key of whitelist) {
+            if (!properties.includes(key as TKey))
+                throw new DeepMockError(`Property "${key}" does not exist on ${name}, so it can't be whitelisted`);
+        }
+    }
+
+    for (const property of properties) {
         if (mockKeys.includes(property as TKey)) {
             mock[property].mockAllowMethod();
             (instance as any)[property] = proxy[property];

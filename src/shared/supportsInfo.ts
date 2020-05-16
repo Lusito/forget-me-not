4dbff368c2849115ad35ffc9a3dc16c378aa117d
Lusito <core@lusito.info>
2020-05-16 17:56:53 +0200

    cleanup

--- src/shared/supportsInfo.ts
+++ src/shared/supportsInfo.ts
@@ -1,33 +1,47 @@
 import { BrowserInfo, BrowserType } from "./browserInfo";
 
+export interface SupportsInfoProps {
+    removeLocalStorageByHostname: boolean;
+    firstPartyIsolation: boolean;
+    requestFilterIncognito: boolean;
+    removeIndexedDbByHostname: boolean;
+    removeServiceWorkersByHostname: boolean;
+}
+
 export class SupportsInfo {
-    public constructor(
-        public readonly removeLocalStorageByHostname: boolean,
-        public readonly firstPartyIsolation: boolean,
-        public readonly requestFilterIncognito: boolean,
-        public readonly removeIndexedDbByHostname: boolean,
-        public readonly removeServiceWorkersByHostname: boolean,
-    ) {}
+    public readonly removeLocalStorageByHostname: boolean;
+
+    public readonly firstPartyIsolation: boolean;
+
+    public readonly requestFilterIncognito: boolean;
+
+    public readonly removeIndexedDbByHostname: boolean;
+
+    public readonly removeServiceWorkersByHostname: boolean;
+
+    public constructor(props: Partial<SupportsInfoProps>) {
+        this.removeLocalStorageByHostname = props.removeLocalStorageByHostname || false;
+        this.firstPartyIsolation = props.firstPartyIsolation || false;
+        this.requestFilterIncognito = props.requestFilterIncognito || false;
+        this.removeIndexedDbByHostname = props.removeIndexedDbByHostname || false;
+        this.removeServiceWorkersByHostname = props.removeServiceWorkersByHostname || false;
+    }
 }
 
 export function getSupports(browserInfo: BrowserInfo) {
     switch (browserInfo.type) {
         case BrowserType.FIREFOX:
-            return new SupportsInfo(
-                browserInfo.versionAsNumber >= 58,
-                browserInfo.versionAsNumber >= 59,
-                browserInfo.versionAsNumber >= 68,
-                browserInfo.versionAsNumber >= 77,
-                browserInfo.versionAsNumber >= 77,
-            );
+            return new SupportsInfo({
+                removeLocalStorageByHostname: browserInfo.versionAsNumber >= 58,
+                firstPartyIsolation: browserInfo.versionAsNumber >= 59,
+                requestFilterIncognito: browserInfo.versionAsNumber >= 68,
+                removeIndexedDbByHostname: browserInfo.versionAsNumber >= 77,
+                removeServiceWorkersByHostname: browserInfo.versionAsNumber >= 77,
+            });
         case BrowserType.FENNEC:
-            return new SupportsInfo(
-                false,
-                browserInfo.versionAsNumber >= 59,
-                false,
-                false,
-                false,
-            );
+            return new SupportsInfo({
+                firstPartyIsolation: browserInfo.versionAsNumber >= 59,
+            });
     }
-    return new SupportsInfo(false, false, false, false, false);
+    return new SupportsInfo({});
 }
